b64a53efaf28a55531e5737d52491614f7f57822
J2mF <mp4-12cs5@hotmail.com>
2020-12-22 10:51:47 +0200

    Create Spring Boot Project

---
+++ code/src/main/java/org/thekiddos/faith/FaithApplication.java
@@ -0,0 +1,13 @@
+package org.thekiddos.faith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaithApplication.class, args);
+    }
+
+}
---
+++ code/src/test/java/org/thekiddos/faith/FaithApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thekiddos.faith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
