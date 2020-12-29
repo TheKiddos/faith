397bd2cba138825e7cf4c3d2f2996ade2bea077e
J2mF <mp4-12cs5@hotmail.com>
2020-12-29 09:20:03 +0200

    Fix maven test not running due to wrong dependency

--- code/src/test/java/org/thekiddos/faith/FaithApplicationTest.java
+++ code/src/test/java/org/thekiddos/faith/FaithApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FaithApplicationTests {
+class FaithApplicationTest {
 
     @Test
     void contextLoads() {
