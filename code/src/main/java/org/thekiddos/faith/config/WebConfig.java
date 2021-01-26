998a098c0ce9fcfee67b192a6ec3e2775eab09ff
J2mF <mp4-12cs5@hotmail.com>
2021-01-26 19:58:38 +0200

    Add Spring Security User Store Config Fixes #24

--- code/src/main/java/org/thekiddos/faith/config/WebConfig.java
+++ code/src/main/java/org/thekiddos/faith/config/WebConfig.java
@@ -1,16 +1,40 @@
 package org.thekiddos.faith.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.thekiddos.faith.services.UserService;
 
 @EnableWebSecurity
 public class WebConfig extends WebSecurityConfigurerAdapter {
-    protected void configure( HttpSecurity http) throws Exception {
+    private final UserService userService;
+
+    @Autowired
+    public WebConfig( UserService userService ) {
+        this.userService = userService;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure( HttpSecurity http ) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/", "/**").permitAll()
                 .and()
                 .formLogin().loginPage("/login");
+    }
+
+    @Override
+    public void configure( AuthenticationManagerBuilder auth ) throws Exception {
+        auth.userDetailsService( userService ).passwordEncoder( bCryptPasswordEncoder() );
     }
 }
