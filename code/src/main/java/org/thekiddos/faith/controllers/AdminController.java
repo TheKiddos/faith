8706067537e0e3808e9210e40998adac1779f01b
J2mF <mp4-12cs5@hotmail.com>
2021-02-16 19:10:47 +0200

    Add Users Admin panel get handler
    Signed-off-by: J2mF <mp4-12cs5@hotmail.com>

--- code/src/main/java/org/thekiddos/faith/controllers/AdminController.java
+++ code/src/main/java/org/thekiddos/faith/controllers/AdminController.java
@@ -1,14 +1,28 @@
 package org.thekiddos.faith.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thekiddos.faith.services.UserService;
 
 @Controller
 @RequestMapping(value = "admin")
 public class AdminController {
+    private final UserService userService;
+
+    public AdminController( UserService userService ) {
+        this.userService = userService;
+    }
+
     @GetMapping
     String getAdminPanel() {
         return "admin/home";
+    }
+
+    @GetMapping( value = "/users" )
+    String getUsersAdminPanel( Model model ) {
+        model.addAttribute( "users", userService.getAll() );
+        return "admin/users";
     }
 }
