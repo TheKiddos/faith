51f8d059f3da6ef5205f9f26b1401469776de096
J2mF <37801310+HHHMHA@users.noreply.github.com>
2021-03-18 22:04:50 +0200

    Add Project Create Feature Test (#65)
    * Add Project Create Feature Test
    
    * Fix findAll return type for Project Repo
    
    * Add missing import in ProjectRepo
    
    * Fix imports in Project Feature plus linting issues
    
    * Fix imports

--- code/src/main/java/org/thekiddos/faith/repositories/ProjectRepository.java
+++ code/src/main/java/org/thekiddos/faith/repositories/ProjectRepository.java
@@ -1,9 +1,11 @@
 package org.thekiddos.faith.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.thekiddos.faith.models.Project;
 
 @Repository
-public interface ProjectRepository extends CrudRepository<Project, Long> {
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findAll();
 }
--- code/src/test/java/org/thekiddos/faith/functional/ProjectFeature.java
+++ code/src/test/java/org/thekiddos/faith/functional/ProjectFeature.java
@@ -1,5 +1,7 @@
 package org.thekiddos.faith.functional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -8,27 +10,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.thekiddos.faith.Utils;
 import org.thekiddos.faith.services.UserService;
+import org.thekiddos.faith.repositories.*;
+import org.thekiddos.faith.models.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ProjectFeature {
     private final UserService userService;
     private final WebDriver webDriver;
-
-    public ProjectFeature( UserService userService, WebDriver webDriver ) {
+    private final ProjectRepository projectRepository;
+    private final User user;
+    
+    @Autowired
+    public ProjectFeature( UserService userService, WebDriver webDriver, ProjectRepository projectRepository ) {
         this.userService = userService;
         this.webDriver = webDriver;
+        this.projectRepository = projectRepository;
+        this.user = Utils.getOrCreateTestUser( userService, "stakeholder@test.com", "Stakeholder" );
     }
 
     @Given( "Stakeholder logins" )
     public void stakeholderLogins() {
-        var email = "stakeholder@test.com";
-        var type = "Stakeholder";
-
-        Utils.getOrCreateTestUser( userService, email, type );
-
         webDriver.get( Utils.LOGIN_PAGE );
-        webDriver.findElement( By.id( "email" ) ).sendKeys( email );
+        webDriver.findElement( By.id( "email" ) ).sendKeys( "stakeholder@test.com" );
         webDriver.findElement( By.id( "password" ) ).sendKeys( "password" );
         webDriver.findElement( By.id( "submit" ) ).click();
     }
@@ -46,25 +50,48 @@ public class ProjectFeature {
 
     @And( "Stakeholder fills project details" )
     public void stakeholderFillsProjectDetails() {
+            webDriver.findElement( By.id( "name" ) ).sendKeys( "new world order" );
+            webDriver.findElement( By.id( "description" ) ).sendKeys( "Make all people slaves" );
+            webDriver.findElement( By.id( "preferredBid" ) ).sendKeys( "200.0" );
+            webDriver.findElement( By.id( "duration" ) ).sendKeys( "31" );
+            webDriver.findElement( By.id( "minimumQualification" ) ).sendKeys( "100" );
     }
 
     @And( "Stakeholder sets the project public" )
     public void stakeholderSetsTheProjectPublic() {
+        webDriver.findElement( By.id( "allowBidding" ) ).click();
     }
 
     @When( "Stakeholder clicks the submit button" )
     public void stakeholderClicksTheSubmitButton() {
+        webDriver.findElement( By.id( "submit" ) ).click();
     }
 
     @Then( "Project is added" )
     public void projectIsAdded() {
+        var projects = projectRepository.findAll();
+        
+        assertFalse( projects.isEmpty() );
+        
+        var project = projects.get( 0 );
+        assertNotNull( project );
+        assertEquals( "new world order", project.getName() );
+        assertEquals( "Make all people slaves", project.getDescription() );
+        assertEquals( 200.0, project.getPreferredBid() );
+        assertEquals( 31, project.getDuration() );
+        assertEquals( 100, project.getMinimumQualification() );
+        assertEquals( true, project.isAllowBidding() );
+        assertEquals( (Stakeholder) user.getType(), project.getOwner() );
     }
 
     @And( "Project is seen on homepage new projects section" )
     public void projectIsSeenOnHomepageNewProjectsSection() {
+        var project = projectRepository.findAll().get( 0 );
+        webDriver.findElement( By.id( "project_" + project.getId() ) );
     }
 
     @And( "Stakeholder sets the project private" )
     public void stakeholderSetsTheProjectPrivate() {
+        // Default value is private so no need to do anything here right?
     }
 }
