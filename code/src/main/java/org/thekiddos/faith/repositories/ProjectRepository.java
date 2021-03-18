dd71cd32c86e81f2bf8412c1b58289fe8e1da612
J2mF <mp4-12cs5@hotmail.com>
2021-03-18 21:57:51 +0200

    Fix imports

--- code/src/main/java/org/thekiddos/faith/repositories/ProjectRepository.java
+++ code/src/main/java/org/thekiddos/faith/repositories/ProjectRepository.java
@@ -1,7 +1,7 @@
 package org.thekiddos.faith.repositories;
 
 import java.util.List;
-import org.springframework.data.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.thekiddos.faith.models.Project;
 
