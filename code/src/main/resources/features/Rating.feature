b8074d8d29b9d4e08b4a70c0eda8f4496e4cbc3e
J2mF <mp4-12cs5@hotmail.com>
2021-07-24 11:00:29 +0300

    Add Freelancer Review Feature (Fixes #47)

---
+++ code/src/main/resources/features/Rating.feature
@@ -0,0 +1,8 @@
+Feature: Rating
+
+  Scenario: Getting Freelancer Rating
+    When Rating User Opens Homepage
+    Then He Can See Each Freelancer Rating
+
+#  TODO:
+#  Scenario: Stakeholder Rates Freelancer
---
+++ code/src/test/java/org/thekiddos/faith/functional/RatingFeature.java
@@ -0,0 +1,37 @@
+package org.thekiddos.faith.functional;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.thekiddos.faith.Utils;
+import org.thekiddos.faith.services.UserService;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RatingFeature {
+    private final WebDriver webDriver;
+    private final UserService userService;
+
+    @Autowired
+    public RatingFeature( WebDriver webDriver, UserService userService ) {
+        this.webDriver = webDriver;
+        this.userService = userService;
+        setUp();
+    }
+
+    private void setUp() {
+        Utils.getOrCreateTestUser( userService, "freelancer@test.com", "Freelancer" );
+    }
+
+    @When( "Rating User Opens Homepage" )
+    public void ratingUserOpensHomepage() {
+        webDriver.manage().window().maximize();
+    }
+
+    @Then( "He Can See Each Freelancer Rating" )
+    public void heCanSeeEachFreelancerRating() {
+        assertTrue( webDriver.findElement( By.className( "rating" ) ).isDisplayed() );
+    }
+}
